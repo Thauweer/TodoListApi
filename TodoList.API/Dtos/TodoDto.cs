6653541adb462f8b8050881495ebaa24ef9d0427
Thauweer <83612707+Thauweer@users.noreply.github.com>
2023-04-26 17:31:31 +0400

    Delete TodoList.API directory

--- TodoList.API/Dtos/TodoDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ToDoListApi.Dtos
-{
-    public class TodoDto
-    {
-        public string? Title { get; set; } = string.Empty;
-        public string? Description { get; set; } = string.Empty;
-        public string? Color { get; set; } = string.Empty;
-    }
-}
